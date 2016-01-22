e8e3acafa1a84045424eb14d85345f3996ef22ba
Sai Charan <charankuppa@gmail.com>
2016-01-22 15:09:33 +0530

    hard disk assignment

--- hardDisk.js
+++ hardDisk.js
@@ -1,53 +1,52 @@
 "use strict"
 
 class Sector {
-	constructor(sectorNumber) {
-		this.sectorNumber = sectorNumber
-	}
-	sizeOfSector() {
-		return 512
-	}
+  constructor(sectorNumber) {
+    this.sectorNumber = sectorNumber
+  }
+  sizeOfSector() {
+    return 512
+  }
 }
 
 class Track {
-	constructor(sectorCollections) {
-		this.sectorCollections = sectorCollections
-		this.initialSize = 0
-	}
-	sizeOfTrack() {
-		this.sectorCollections.forEach((eachSector) => {
-			this.initialSize += eachSector.sizeOfSector()
-		})		
-		return this.initialSize	
-	}
+  constructor(sectorCollections) {
+    this.sectorCollections = sectorCollections
+    this.initialSize = 0
+  }
+  sizeOfTrack() {
+    this.sectorCollections.forEach((eachSector) => {
+	this.initialSize += eachSector.sizeOfSector()
+    })		
+    return this.initialSize	
+  }
 }
 
 class Platter {
-	constructor(trackCollections) {
-		this.trackCollections = trackCollections
-		this.initialSizeOfPlatter = 0
-	}
-	sizeOfPlatter() {
-		this.trackCollections.forEach((eachTrack) => {
-			this.initialSizeOfPlatter += eachTrack.sizeOfTrack()
-		})		
-		return this.initialSizeOfPlatter
-	}
+  constructor(trackCollections) {
+    this.trackCollections = trackCollections
+    this.initialSizeOfPlatter = 0
+  }
+  sizeOfPlatter() {
+    this.trackCollections.forEach((eachTrack) => {
+	this.initialSizeOfPlatter += eachTrack.sizeOfTrack()
+    })		
+    return this.initialSizeOfPlatter
+  }
 }
 
 class Size {
-	constructor(platterCollections) {
-		this.platterCollections = platterCollections
-		this.initialSizeOfMainDisk = 0
-	}
-	sizeOfDisk() {
-		this.platterCollections.forEach((eachPlatter) => {
-			this.initialSizeOfMainDisk += eachPlatter.sizeOfPlatter()
-		})		
-		return this.initialSizeOfMainDisk
-	}	
+  constructor(platterCollections) {
+    this.platterCollections = platterCollections
+    this.initialSizeOfMainDisk = 0
+  }
+  sizeOfDisk() {
+    this.platterCollections.forEach((eachPlatter) => {
+	this.initialSizeOfMainDisk += eachPlatter.sizeOfPlatter()
+    })		
+    return this.initialSizeOfMainDisk
+  }	
 }
-
 
 let sector1 = new Sector(1)
 let sector2 = new Sector(2)
@@ -64,4 +63,4 @@ let platter2 = new Platter([track3])
 let diskSize = new Size([platter1,platter2])
 let size = diskSize.sizeOfDisk()
 
-console.log('The size of the hard disk is ' + size/1024 +'MB')
+console.log('The size of the hard disk is ' + size/1024 +'KB')
